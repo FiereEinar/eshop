94b329ccffa1ef02f53fffbcb2f2723ec9b78674
Nick Mours <nickxylanmelloria@gmail.com>
2025-03-19 08:59:15 +0800

    client side setup

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from './components/ui/button';
+
+function App() {
+	return (
+		<main className='p-3'>
+			<h1 className='text-red-500'>Hello World!</h1>
+			<Button size='sm'>Submit</Button>
+		</main>
+	);
+}
+
+export default App;
